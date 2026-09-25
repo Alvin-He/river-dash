0556ae1d973256cf49a150e85503721e5f59b21b
tlcriq <tatecriqui@gmail.com>
2026-09-25 16:03:41 -0700

    Initial backend setup

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-	println!("Hello, world!");
+/*
+	Structure guide
+	1. Using tokio & axum for the web framework. This lets us set up routing
+	2. Using Serde for converting to and from JSON. Lets us (de)serialize data
+	
+*/
+
+use axum::{
+    extract::{Path, State},
+    routing::{get, post},
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
+ 
+#[derive(Debug, Deserialize, Serialize, Clone)]
+struct User {
+    id: String,
+    name: String, // May be combined with id
+	role: String
+}
+
+
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+ 
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("Listening on http://localhost:3000");
+    axum::serve(listener, app).await.unwrap();
 }
